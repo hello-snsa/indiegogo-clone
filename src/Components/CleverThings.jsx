0ae6de4e5120d1998e408b9aedbe0b07e979a66b
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-23 22:11:01 +0530

    Update CleverThings.jsx

---
+++ src/Components/CleverThings.jsx
@@ -0,0 +1,31 @@
+
+export default function CleverThings(){
+
+
+    return(
+        <>
+    <div className="cleverThingsMainDiv flex">
+
+        <div className="cleverImageDiv">
+            <img src="https://c3.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fit,w_275,g_center,q_auto:best,dpr_1.3,f_auto/homepage/crowdfunding-crowdsurfing_base.svg" alt="image" />
+        </div>
+
+        <div className="cleverTextDiv">
+        <div className="cleverHeaderDiv w-75 tAlign-C">
+       <h1> Clever Things For Curious Humans™</h1>
+<p>There’s no better place to start the hunt for something new and special. Begin on Indiegogo to find clever and unconventional things that solve everyday problems large and small.</p>
+
+<div className="cleverButtonDiv">
+    <button id="cleverSignup">SIGN UP NOW</button>
+    <button id="cleverLearnMore">LEARN MORE</button>
+</div>
+        </div>
+
+        </div>
+
+
+
+    </div>
+        </>
+    )
+}
