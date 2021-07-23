c560bfe4ec7921b9d4701252270a9acea948bf96
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-23 22:11:16 +0530

    Create HomepageReview.jsx

---
+++ src/Components/HomepageReview.jsx
@@ -0,0 +1,60 @@
+
+
+
+export default function HomepageReview(){
+
+    let rightArrow=">";
+    return(
+        <>  
+        <div className="homepageReviewMainDiv">
+
+        <div className="hpReviewHeaderDiv">
+            <h1>From the Indiegogo Review</h1>
+            <p>Your behind-the-scenes view of the people and stories behind Indiegogo projects</p>
+        </div>
+        
+        <div className="hpReviewCardDiv flex">
+            <div className="hprCard1">
+                <div className="hprc1ImageDiv">
+                    <img src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1625773543/mds3fceo0ks3cio0fcik.png" alt="review image" />
+                </div>
+                <div className="hpriHeaderDiv">
+                    <h3>5 Gadgets To Make You Feel At Home, In Your Home</h3>
+                    <p>Check out these Indiegogo products that will make your space more fun and more livable</p>
+                    <p className="readMore">READ MORE {rightArrow} </p>
+                </div>
+
+            </div>
+{/*  */}
+<div className="hprCard1">
+                <div className="hprc1ImageDiv">
+                    <img src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1625773543/mds3fceo0ks3cio0fcik.png" alt="review image" />
+                </div>
+                <div className="hpriHeaderDiv">
+                    <h3>5 Gadgets To Make You Feel At Home, In Your Home</h3>
+                    <p>Check out these Indiegogo products that will make your space more fun and more livable</p>
+                    <p className="readMore">READ MORE {rightArrow} </p>
+                </div>
+
+            </div>
+{/*  */}
+<div className="hprCard1">
+                <div className="hprc1ImageDiv">
+                    <img src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1625773543/mds3fceo0ks3cio0fcik.png" alt="review image" />
+                </div>
+                <div className="hpriHeaderDiv">
+                    <h3>5 Gadgets To Make You Feel At Home, In Your Home</h3>
+                    <p>Check out these Indiegogo products that will make your space more fun and more livable</p>
+                    <p className="readMore">READ MORE {rightArrow} </p>
+                </div>
+
+            </div>
+{/*  */}
+        </div>
+
+
+
+        </div>
+        </>
+    )
+}
