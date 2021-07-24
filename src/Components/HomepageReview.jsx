cff33c212a9d5a16fa7851bd7bae5012bf358068
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-24 14:51:27 +0530

    Update HomepageReview.jsx

--- src/Components/HomepageReview.jsx
+++ src/Components/HomepageReview.jsx
@@ -16,36 +16,36 @@ export default function HomepageReview(){
         <div className="hpReviewCardDiv flex">
             <div className="hprCard1">
                 <div className="hprc1ImageDiv">
-                    <img src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1625773543/mds3fceo0ks3cio0fcik.png" alt="review image" />
+                    <img src="./review1.png" alt="review image" />
                 </div>
                 <div className="hpriHeaderDiv">
                     <h3>5 Gadgets To Make You Feel At Home, In Your Home</h3>
                     <p>Check out these Indiegogo products that will make your space more fun and more livable</p>
-                    <p className="readMore">READ MORE {rightArrow} </p>
+                    <p className="readMore bold">READ MORE {rightArrow} </p>
                 </div>
 
             </div>
 {/*  */}
 <div className="hprCard1">
                 <div className="hprc1ImageDiv">
-                    <img src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1625773543/mds3fceo0ks3cio0fcik.png" alt="review image" />
+                    <img src="./review2.png" alt="review image" />
                 </div>
                 <div className="hpriHeaderDiv">
-                    <h3>5 Gadgets To Make You Feel At Home, In Your Home</h3>
-                    <p>Check out these Indiegogo products that will make your space more fun and more livable</p>
-                    <p className="readMore">READ MORE {rightArrow} </p>
+                    <h3>Most Popular Campaign in Every State</h3>
+                    <p>Ever wonder which crowdfunding campaigns raised the most funds in the US?</p>
+                    <p className="readMore bold">READ MORE {rightArrow} </p>
                 </div>
 
             </div>
 {/*  */}
-<div className="hprCard1">
+<div className="hprCard1" id="review3">
                 <div className="hprc1ImageDiv">
-                    <img src="https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1625773543/mds3fceo0ks3cio0fcik.png" alt="review image" />
+                    <img src="./review3.png" alt="review image"  />
                 </div>
                 <div className="hpriHeaderDiv">
-                    <h3>5 Gadgets To Make You Feel At Home, In Your Home</h3>
-                    <p>Check out these Indiegogo products that will make your space more fun and more livable</p>
-                    <p className="readMore">READ MORE {rightArrow} </p>
+                    <h3>8 Green Tech Ideas to Make the Planet Cleane</h3>
+                    <p>Here are some of the most innovative eco-conscious campaigns on Indiegogo right now.</p>
+                    <p className="readMore bold">READ MORE {rightArrow} </p>
                 </div>
 
             </div>
