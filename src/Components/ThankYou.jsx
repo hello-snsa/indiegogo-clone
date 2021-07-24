83abf40259bfb84ba191fce6237be9d45396cc65
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-24 14:51:31 +0530

    Create ThankYou.jsx

---
+++ src/Components/ThankYou.jsx
@@ -0,0 +1,28 @@
+
+export default function ThankYou(){
+
+    return(
+    
+    <>
+    <div className="thankYouMainDiv">
+        <div>
+            <div className="thankyouHeader">
+                <h1>Thank You for your Support.</h1>
+            <h3>Your Transaction Number is <span id="trxn">10IN12335675649pW</span></h3>
+            <p>For any query reach us : <span id="mail">help@indiegogo.com</span></p></div>
+            <button id="backToHome">BACK TO HOMEPAGE</button>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        </div>
+        </>
+        )
+}
