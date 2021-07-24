9c90f87677006f1276ecf65a9261950d05a53847
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-24 14:51:18 +0530

    Create HomePage.jsx

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,45 @@
+import CarouselImageBox from "./CarouselImageBox";
+import CarouselTicketPart from "./CarouselTicketPart";
+import CleverThings from "./CleverThings";
+import DonationPage from "./DonationPage";
+import DonationPayment from "./DonationPayment";
+import Footer from "./Footer";
+import HomepageCollections from "./HomepageCollections";
+import HomepageReview from "./HomepageReview";
+import IntrestYou from "./IntrestYou";
+import Navbar from "./Navbar";
+import HomepagePopularProject from "./PopularProject";
+import TakeRide from "./TakeRide";
+import ThankYou from "./ThankYou";
+
+
+
+export default function HomePage() {
+  return (
+    <>
+
+<div className="mainDiv">
+
+<Navbar />
+
+<CarouselImageBox />
+<CarouselTicketPart />
+<HomepagePopularProject />
+<HomepageCollections />
+
+<TakeRide />
+<IntrestYou />
+
+<HomepageReview />
+
+<CleverThings />
+
+
+
+<Footer />
+
+</div>{/* MainDiv */}
+    </>
+  );
+}
+
