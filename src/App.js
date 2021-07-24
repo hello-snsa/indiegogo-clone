e8fd1c402ad892a60bfd62c09e2f3a8269a23d58
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-07-24 14:51:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,28 @@
 
 import './App.css';
-
-import ATempFile from './Components/ATempFile';
-import Navbar from './Components/Navbar';
-import CarouselImageBox from './Components/CarouselImageBox'
-import CarouselTicketPart from './Components/CarouselTicketPart';
-import Footer from './Components/Footer';
-import HomepageCollections from './Components/HomepageCollections';
-import TakeRide from './Components/TakeRide';
-import IntrestYou from './Components/IntrestYou';
-import CleverThings from './Components/CleverThings';
-import HomepageReview from './Components/HomepageReview';
-import HomepagePopularProject from './Components/PopularProject';
 import DonationPage from './Components/DonationPage';
-import DonationPayment from './Components/DonationPayment';
+import HomePage from './Components/HomePage';
+import DonationPayment from './Components/DonationPayment'
+import ThankYou from './Components/ThankYou'
 
 function App() {
   return (
     <>
-{/* <ATempFile /> */}
+
 <div className="mainDiv">
+  
+   <HomePage />
 
-<Navbar />
 
-<CarouselImageBox />
-<CarouselTicketPart />
-<HomepagePopularProject />
-<HomepageCollections />
+   {/* <DonationPage data="" /> */}
 
-<TakeRide />
-<IntrestYou />
+{/* <DonationPayment /> */}
 
-<HomepageReview />
+{/* <ThankYou /> */}
 
-<CleverThings />
-
-<DonationPage data="" />
-
-<DonationPayment />
-
-<Footer />
-
-</div>{/* MainDiv */}
-    </>
+   
+   </div>
+   </> 
   );
 }
 
